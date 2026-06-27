9e7772c028fd0ca2e4f90a19950b03410aaf2004
Josh Laidlaw <josh.laidlaw@gmail.com>
2026-06-27 10:31:03 -0700

    Switch to pnpm for package manager

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
   webServer: {
-    command: 'npm run dev -- --host 127.0.0.1 --port 4321',
+    command: 'pnpm run dev -- --host 127.0.0.1 --port 4321',
     url: 'http://127.0.0.1:4321',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
